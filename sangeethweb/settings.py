e76b28f0c0c11d5d73b57c80bd61f0c8bec1046a
Suhas S Bhandary <bhandarysuhas777@gmail.com>
2023-10-18 00:51:39 +0530

    Deployed Home Page

--- sangeethweb/settings.py
+++ sangeethweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(bj@73#r$+x5_0$!yq*4bg6(yp31e811)y)wa1$5tep2x2@sq=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landingpage'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

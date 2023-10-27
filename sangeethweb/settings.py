2cfd5fd0f37b79b8b28efb81873be57b247ef1d7
Suhas S Bhandary <bhandarysuhas777@gmail.com>
2023-10-27 19:42:20 +0530

    Heroku Deploy2

--- sangeethweb/settings.py
+++ sangeethweb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(bj@73#r$+x5_0$!yq*4bg6(yp31e811)y)wa1$5tep2x2@sq='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

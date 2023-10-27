932752f858d9a5c5bde060948a225d30d23e586f
Suhas S Bhandary <bhandarysuhas777@gmail.com>
2023-10-27 19:33:16 +0530

    Heroku Deploy

--- sangeethweb/settings.py
+++ sangeethweb/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,5 +123,5 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

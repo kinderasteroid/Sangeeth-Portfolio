8fa3b2df4a3357541d763027318bfac1f9e17b0b
Suhas S Bhandary <bhandarysuhas777@gmail.com>
2023-10-27 19:45:15 +0530

    Heroku Deploy3

--- sangeethweb/settings.py
+++ sangeethweb/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
@@ -120,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'productionfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 django_heroku.settings(locals())
